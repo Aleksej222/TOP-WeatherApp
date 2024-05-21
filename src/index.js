5c31b0faedd95361e82243453f721d9b2f3474af
Aleksej222 <aleksejsuzic2001@gmail.com>
2024-05-21 20:07:17 +0200

    Fixed return object pending.

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ btnSendRequest.addEventListener('click', () => {
 
     
 
-    let weatherData = sendRequest();
+    sendRequest();
 
-    console.log(weatherData);  // promise pending
+    //console.log(weatherData);  // promise pending
         
 
 });
@@ -22,88 +22,21 @@ async function sendRequest() {
     let response = await fetch('https://api.weatherapi.com/v1/current.json?key=ba2ccfa3d69b4712b5e74013241405&q=london&contentType=json');
     let data = await response.json();
 
-    // let weatherData = data.current;
-    let { weatherData } = data;
-
-    console.log(data);
+    let weatherData = data.current;
+  
     // console.log(weatherData);
 
-    return weatherData;
+    setWeather(weatherData);
+
+    // return weatherData;
 
 }
 
-function sendRequest2() {
+function setWeather(weatherData) {
 
-    const apiRequest = async () => {
-
-        const data = await fetch(
-            'https://api.weatherapi.com/v1/current.json?key=ba2ccfa3d69b4712b5e74013241405&q=london&contentType=json'
-        );
-
-        const weather = await data.json();
-
-        console.log(weather);
-        console.log('1');
-    }
-
-    console.log(apiRequest);
-    return apiRequest;
+    console.log(weatherData);
 
 }
-
-
-/*
-
-    // Returns coordinates and city name for a specified city name.
-    async function getCoords(url) {
-    const response = await fetch(url);
-    const weatherData = await response.json();
-    const { coord } = weatherData;
-    coord.name = weatherData.name;
-    coord.country = weatherData.sys.country;
-    // console.log(weatherData);
-    // console.log(weatherData.weather);
-
-    return coord;
-
-}
-
-*/
-
-/* 
-
-
-        const data = await fetch('https://api.weatherapi.com/v1/current.json?key=ba2ccfa3d69b4712b5e74013241405&q=london&contentType=json')
-        .then(function(response) {
-    
-            //let data = response.json();
-            //data = data.current;
-            
-            let data = JSON.stringify(response);
-    
-            console.log(data);
-            // console.log(response);   
-    
-        })
-
-    }
-
-*/
-
-
-/*
-
-    const fetchWeather = async () => {
-    const data = await fetch(
-        "https://api.openweathermap.org/data/2.5/weather?q=Islamabad&units=metric&appid=" +
-        APIKey
-    );
-    const weather = await data.json();
-    setWeather(weather);
-};
-
-*/
-
 
 /*
     TODO: Validate input field

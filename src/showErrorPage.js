f8fa889c0278a82d0ef18ecb18fd855368205780
Aleksej222 <aleksejsuzic2001@gmail.com>
2024-09-06 12:58:39 +0200

    Testing

--- src/showErrorPage.js
+++
@@ -1,8 +0,0 @@
-export function showErrorPage() {
-    
-    const contentHtml = document.querySelector('.content');
-    contentHtml.style.display = 'none';
-
-    const errorPageHtml = document.querySelector('.not-found');
-    errorPageHtml.style.display = 'flex';
-}
